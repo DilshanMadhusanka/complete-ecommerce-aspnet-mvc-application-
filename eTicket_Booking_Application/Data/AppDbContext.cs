96362eb17e0e27b5ffd4d33ddbba7e660b2a3e5e
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-12-21 13:39:40 +0530

    Create Dbcontext

--- eTicket_Booking_Application/Data/AppDbContext.cs
+++ eTicket_Booking_Application/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eTicket_Booking_Application.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace eTicket_Booking_Application.Data
@@ -15,5 +16,35 @@ namespace eTicket_Booking_Application.Data
         {
             
         }
+
+
+        // over ride onModelCreating 
+        // this is important when creating the default authentiaction table 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne( m =>m.Movie).WithMany
+                (am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany
+               (am => am.Actors_Movies).HasForeignKey(m => m.ActorId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        // define table names for each models
+        public DbSet<Actor> Actors { get; set; }   // Actor kiyanne model name eka. Actors kiyanne table ekat dana name eka 
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        
     }
 }
